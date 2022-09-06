ab291346d899b590a41468099d3d03f5d478846d
Aleksey Pechurin <pe4urin@yandex.ru>
2022-09-06 22:20:59 +0300

    Доработано мобильное меню

--- js/script.js
+++ js/script.js
@@ -2,16 +2,27 @@
 const menu = document.querySelector('.header__nav');
 const menuBtn = document.querySelector('.menu-btn');
 const menuLink = document.querySelectorAll('.header__link');
+const menuOverlay = document.querySelector('.menu-overlay');
 
-  menuBtn.onclick = function() {
-    menu.classList.toggle('active');
-    menuBtn.classList.toggle('active');
-    document.body.classList.toggle('lock')
-  };
+ menuBtn.onclick = function() {
+  menu.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menuOverlay.classList.toggle('active');
+  document.body.classList.toggle('lock');
+};
 
 for(let i = menuLink.length; i--;) {
   menuLink[i].onclick = function() {
     menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+    document.body.classList.toggle('lock');
   }
+}
+
+menuOverlay.onclick = function() {
+  menu.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+  menuOverlay.classList.toggle('active');
+  document.body.classList.toggle('lock');
 }
